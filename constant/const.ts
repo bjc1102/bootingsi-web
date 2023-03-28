53a8996d7b14cf5b52aa9a4be18b091cb9c47f72
bjc1102 <bjc30849@gmail.com>
2023-03-28 21:50:53 +0900

    fix: const 삭제

--- constant/const.ts
+++ constant/const.ts
@@ -2,6 +2,4 @@ export const token = 'access-token' as const
 export const refreshToken = 'refresh-token' as const
 export const siteURL = 'siteURL'
 export const domain =
-  process.env.NODE_ENV === 'production'
-    ? '.clip-planet.site'
-    : ('localhost' as const)
+  process.env.NODE_ENV === 'production' ? '.clip-planet.site' : 'localhost'
